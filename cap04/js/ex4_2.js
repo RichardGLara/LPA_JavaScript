5fec80819761c67e669684d4ae425986ecc4f559
RichardLara <richardgrlara@gmail.com>
2024-07-04 18:58:36 -0300

    Update ex4_2.js file...

--- cap04/js/ex4_2.js
+++ cap04/js/ex4_2.js
@@ -10,12 +10,18 @@ form.addEventListener("submit", (e) => {
     const masculino = form.inMasculino.checked
     const altura = Number(form.inAltura.value)
 
-    let peso                                                                //Declava a variavel peso
+    /*let peso                                                                //Declava a variavel peso
     if (masculino) {                                                        //Se MASCULINO == TRUE
         peso = 22 * Math.pow(altura, 2)
     } else {
         peso = 21 * Math.pow(altura, 2)
-    }
+    }*/
+
+    const peso = masculino ? 22 * Math.pow(altura, 2) : 21 * Math.pow(altura, 2)
+    //OPERADOR TERNÁRIO
+    /* Variavel PESO sera se MASCULINO:
+    TRUE = 22 X ALTURA²
+    FALSE = 21 X ALTURA² */
 
     resp.innerText = `${nome}: Seu peso ideal é ${peso.toFixed(3)} Kg`
     resp.style.color = "#ff9900"
